d84b32d1fee812cf5c115a32e2b94e965a6dee65
Jada Grant <jagra27@morgan.edu>
2020-04-01 13:43:50 -0400

    completed required features

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -16,13 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+
         Parse.initialize(
             with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
                 configuration.applicationId = "Parstagram"
                 configuration.server = "https://whispering-bastion-26599.herokuapp.com/parse"
             })
         )
+        
         return true
     }
 
